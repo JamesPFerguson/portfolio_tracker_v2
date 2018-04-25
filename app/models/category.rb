d2cfde0907adff1a73bd0bc43f2cec1fd6f11ba1
James Ferguson <ncjamesferguson@gmail.com>
2018-04-25 18:30:44 +0000

    updated model relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :stocks, through: :stock_categories
+  has_many :stock_categories
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,5 @@
 class Stock < ActiveRecord::Base
   has_many :categories, through: :stock_category
   has_many :stock_categories
+  has_many :portfolios, through: :portfolio_stocks
 end
