4884a878438efc39dcd1997774fff6e0c3c488a1
James Ferguson <ncjamesferguson@gmail.com>
2018-04-30 00:34:38 +0000

    added CRUD actions for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,37 @@
 class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def index
+    @categories = Category.all
+  end
+
+  def create
+    category = category.new(category_params)
+    if category.save
+      redirect_to categories_path
+    else
+      render 'create'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+    if category = Category.find_by(id: params[:category][:id])
+      category.delete
+      redirect_to categories_path
+    else
+      redirect_to categories_path
+    end
+  end
+
 end
