2eea32d91c27c4602f70fe461c425cc0eb03d6ca
James Ferguson <ncjamesferguson@gmail.com>
2018-05-01 11:50:26 +0000

    started building out ticker scraper and figuring out correct css selectors

--- app/lib/scraper.rb
+++ app/lib/scraper.rb
@@ -1,9 +1,43 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 class Scraper
 
 
   def scrape_ticker(ticker)
-    stock = Nokogiri::HTML(open("https://finviz.com/quote.ashx?t=#{ticker}"))
 
+    stock = Stock.new(ticker: "#{ticker}")
+
+    stock_info = Nokogiri::HTML(open("https://finviz.com/quote.ashx?t=#{ticker}"))
+
+    stock_info.css(".snapshot-table2").each do |td|
+      i = 0
+      td.css(".snapshot-td2").each do |cell|
+        cell.text
+        binding.pry
+
+        case i
+        when 1
+          stock.pe_ratio = cell.text
+        when 6
+          if cell.text.include?("B")
+            stock.market_cap = 1000000000 * market_cap_to_number(cell.text)
+          else
+            stock.market_cap = 1000000 * market_cap_to_number(cell.text)
+          end
+
+
+
+        end #ends the case
+      end #ends the second each statement
+    end #ends the first each statement
+
+
+  end #ends the scrape method
+
+
+  def market_cap_to_number(cap)
+    cap.chop.to_f
   end
-
 end
