08141055867acefea2246f8bfd3776e9e4b449c3
James Ferguson <ncjamesferguson@gmail.com>
2018-05-01 18:53:28 +0000

    got css selectors on scraper and started stock new view

--- app/lib/scraper.rb
+++ app/lib/scraper.rb
@@ -9,10 +9,13 @@ class Scraper
 
     stock = Stock.new(ticker: "#{ticker}")
 
+    stock.ticker = ticker
+
     stock_info = Nokogiri::HTML(open("https://finviz.com/quote.ashx?t=#{ticker}"))
 
     stock_info.css(".snapshot-table2").each do |td|
       i = 0
+      byebug
       td.css(".snapshot-td2").each do |cell|
         cell.text
         case i
@@ -25,13 +28,14 @@ class Scraper
             stock.market_cap = 1000000 * market_cap_to_number(cell.text)
           end
         when 23
-
-
+          stock.six_month_appreciation = cell.text
+        when 65
+          stock.price = cell.text
         end #ends the case
       end #ends the second each statement
     end #ends the first each statement
 
-
+    stock.save
   end #ends the scrape method
 
 
