5a259ae979b40e6fc2d343e5fdb983ecf01da33a
James Ferguson <ncjamesferguson@gmail.com>
2018-05-16 07:14:08 +0000

    reordered if statements in account creation action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,23 +6,32 @@ class SessionsController < ApplicationController
 
    def create
 
-     if @user = User.find_by(name: params[:user][:name])
-       if @user.authenticate(params[:password])
-         session[:user_id] = @user.id
-         redirect_to portfolio_path(@user.portfolio)
-       else
-         if !@user
-           flash[:message] = "Username not found"
-         elsif !@user.authenticate(params: [:password])
-           flash[:message] = "password is incorrect"
-          end
-         render 'new'
-       end
-     elsif auth
+     if auth
+
        @user = User.find_or_create_by(uid: auth['uid']) do |u|
          u.name = auth['info']['name']
          u.email = auth['info']['email']
        end
+
+       session[:user_id] = @user.id
+
+     elsif @user = User.find_by(name: params[:user][:name])
+
+       if @user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to portfolio_path(@user.portfolio)
+       else
+         if !@user.authenticate(params: [:password])
+           flash[:notice] = "password is incorrect"
+          end
+         render 'new'
+       end
+
+     elsif !@user
+         flash[:notice] = "Username not found"
+         render 'new'
+       end
+     else
        session[:user_id] = @user.id
        redirect_to portfolio_path(@user.portfolio)
      else
