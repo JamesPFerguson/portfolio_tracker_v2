bca99cc59a4c979ff02c6649c02270436d3ac15e
James Ferguson <ncjamesferguson@gmail.com>
2018-05-18 18:53:29 +0000

    Added activerecord class methods to stock

--- app/models/portfolio_stock.rb
+++ app/models/portfolio_stock.rb
@@ -1,4 +1,5 @@
 class PortfolioStock < ActiveRecord::Base
   belongs_to :stock
   belongs_to :portfolio
+
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,12 @@ class Stock < ActiveRecord::Base
   has_many :categories, through: :stock_category
   has_many :stock_categories
   has_many :portfolios, through: :portfolio_stocks
+
+  def self.cheapest_stock
+    where("pe_ratio > 0").order(pe_ratio :asc).first
+  end
+
+  def self.highest_momentum_value_stock
+    where("category = value").order(six_month_appreciation :desc).first
+  end
 end
