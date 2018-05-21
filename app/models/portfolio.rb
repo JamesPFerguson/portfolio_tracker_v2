7fd289bd442f8fdf40bab673ecd78c23e8f22c79
James Ferguson <ncjamesferguson@gmail.com>
2018-05-21 16:48:57 +0000

    added value method to portfolio model and added to show page

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,13 @@ class Portfolio < ActiveRecord::Base
   has_many :portfolio_stocks
   belongs_to :user
 
+
+  def value
+    p_value = 0
+    self.portfolio_stocks.each do |p_stock|
+        p_value += p_stock.quantity * Stock.find(p_stock.stock_id).price
+    end
+    p_value
+  end
+
 end
