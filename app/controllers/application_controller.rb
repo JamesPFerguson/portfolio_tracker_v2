8a710879635efecc3425f6df12d40f8d0628c339
James Ferguson <ncjamesferguson@gmail.com>
2018-06-15 05:28:09 +0000

    added require login to controller and validation to portfolio stock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   helper :all
+  before_action :require_login
+
   protect_from_forgery with: :exception
   # before_action :require_login
 
--- app/models/portfolio_stock.rb
+++ app/models/portfolio_stock.rb
@@ -3,6 +3,8 @@ class PortfolioStock < ActiveRecord::Base
   belongs_to :portfolio
   accepts_nested_attributes_for :stock
 
+  validates :quantity, :numericality => { :greater_than_or_equal_to => 1 }
+
   def ticker
     self.stock.ticker
   end
