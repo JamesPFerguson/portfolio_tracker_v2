3cd2122d0edad4198a33dc50f0cd2e31921381c8
jamespferguson <ncjamesferguson@gmail.com>
2018-09-23 20:24:14 -0400

    add client form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_201414) do
+ActiveRecord::Schema.define(version: 2018_09_23_191224) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
